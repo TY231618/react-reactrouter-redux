d51e83d117582620c71503071f31ddec9319a78d
Tony <anthony.c.young18@gmail.com>
2018-04-26 16:40:36 +0100

    added reducers for create and update

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,10 +1,10 @@
-import { LOAD_COURSES_SUCCESS } from '../actions/actionTypes';
+import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default (state = initialState.courses, action) => {
 
   switch(action.type) {
-    case LOAD_COURSES_SUCCESS:
+    case types.LOAD_COURSES_SUCCESS:
 
       // debugger;
       // bad mutable way
@@ -13,6 +13,17 @@ export default (state = initialState.courses, action) => {
 
       // good immutable way
       return action.courses;
+
+    case types.CREATE_COURSE_SUCCESS:
+      return [
+        ...state, Object.assign({}, action.course)
+      ];
+
+    case types.UPDATE_COURSE_SUCCESS:
+      return [
+        ...state.filter(course => course.id !== action.course.id), 
+        Object.assign({}, action.course)
+      ];
     
     default: 
       return state;
